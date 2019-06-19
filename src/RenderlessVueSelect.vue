ae58cab61771dadb642d6a433daabddc84f844b6
Renato de Leão <renatodeleao@gmail.com>
2019-06-19 17:56:25 +0100

    Scrollback to current selected after failed fusy search

--- src/RenderlessVueSelect.vue
+++ src/RenderlessVueSelect.vue
@@ -202,6 +202,9 @@ export default {
       } else {
         this.query = value.label;
       }
+      this.$nextTick(() => {
+        this._scrollListToHighlighted('instant');
+      })
     },
 
     /**
